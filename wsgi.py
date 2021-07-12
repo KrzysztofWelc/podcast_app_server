1f8d037fb95418574d9e3b9e7bc7381cb6b85ac0
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-07-12 22:56:03 +0200

    pp

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app import create_app
-import app
-application = create_app()
+
+application = create_app(config='app.config.ProductionConfig')
 
 if __name__ == "__main__":
  application.run()
